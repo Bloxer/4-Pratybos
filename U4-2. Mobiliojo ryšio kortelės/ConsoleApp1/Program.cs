49a11e948a8a384c5601c9228879aacd89d0cb0c
Bloxer <danasniedzis@gmail.com>
2022-11-23 03:25:10 +0200

    Add files via upload

---
+++ U4-2. Mobiliojo ryšio kortelės/ConsoleApp1/Program.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Globalization;
+
+namespace lab3
+{
+    /// <summary>
+    /// klasė žadėjui aprašyti
+    /// </summary>
+    class Zaidejas
+    {
+        string komanda;
+        string pavarde;
+        string vardas;
+        int RungtSk; // sužaistų rungtynių skaičius
+        int ImustSk; // įmuštų įvarčių skaičius
+        bool Daug;
+        /// <summary>
+        /// pradiniai žaidėjo duomenys
+        /// </summary>
+        /// <param name="komanda"></param>
+        /// <param name="pavarde"></param>
+        /// <param name="vardas"></param>
+        /// <param name="rungtSk"></param>
+        /// <param name="imustSk"></param>
+        public Zaidejas(string komanda, string pavarde, string vardas, int rungtSk, int imustSk)
+        {
+            this.komanda = komanda;
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            RungtSk = rungtSk;
+            ImustSk = imustSk;
+            Daug = true;
+        }
+
+        /// <summary>
+        /// spausdinimo metodas
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string eilute;
+            eilute = string.Format("{0, -12} | {1, -15} | {2, -9} |    {3,8}    | {4, 9} |  ",
+            komanda, pavarde, vardas, RungtSk, ImustSk);
+            return eilute;
+        }
+        public string ImtiKomanda() { return komanda; }
+        public string ImtiVarda() { return vardas; }
+        public int ImtiRungt() { return RungtSk; }
+        public int ImtiIvarciai() { return ImustSk; }
+        public void KeistiDaug(bool a) { Daug = a; }
+        public bool ImtiDaug() { return Daug; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Zaidejas zaidejas &&
+                   komanda == zaidejas.komanda &&
+                   pavarde == zaidejas.pavarde &&
+                   vardas == zaidejas.vardas &&
+                   RungtSk == zaidejas.RungtSk &&
+                   ImustSk == zaidejas.ImustSk &&
+                   Daug == zaidejas.Daug;
+        }
+        /// <summary>
+        /// užklotas metodas
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            int hashCode = 741578156;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(komanda);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(pavarde);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(vardas);
+            hashCode = hashCode * -1521134295 + RungtSk.GetHashCode();
+            hashCode = hashCode * -1521134295 + ImustSk.GetHashCode();
+            hashCode = hashCode * -1521134295 + Daug.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator <=(Zaidejas s1, Zaidejas s2)
+        {
+            int st = string.Compare(s1.komanda, s2.komanda, StringComparison.CurrentCulture);
+            int st2 = string.Compare(s1.vardas, s2.vardas, StringComparison.CurrentCulture);
+            return (st < 0) || ((st == 0) && (st2 < 0));
+        }
+        public static bool operator >=(Zaidejas s1, Zaidejas s2)
+        {
+            int st = string.Compare(s1.komanda, s2.komanda, StringComparison.CurrentCulture);
+            int st2 = string.Compare(s1.vardas, s2.vardas, StringComparison.CurrentCulture);
+            return (st > 0) || ((st == 0) && (st2 > 0));
+        }
+    }
+    /// <summary>
+    /// Futbolininkų konteinerinė klasė
+    /// </summary>
+    class Futbolininkai
+    {
+        const int CMax = 100;
+        private Zaidejas[] z;
+        private int kiek;
+        public Futbolininkai()
+        {
+            kiek = 0;
+            z = new Zaidejas[CMax];
+        }
+        /// <summary>
+        /// grąžina kiekį
+        /// </summary>
+        /// <returns></returns>
+        public int imtiKiek() { return kiek; }
+        /// <summary>
+        /// grąžina nurodyto indekso žaidėjo objektą
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public Zaidejas Imti(int i) { return z[i]; }
+        /// <summary>
+        /// padeda į žaidėjo masyvą naują žaidėją ir masyvo dydi padidina vienetu
+        /// </summary>
+        /// <param name="ob"></param>
+        public void Dėti(Zaidejas ob) { z[kiek++] = ob; }
+
+        /// <summary>
+        /// rikiavimo metodas
+        /// </summary>
+        public void Rikiuoti()
+        {
+            Futbolininkai Fut = new Futbolininkai();
+            for (int i = 0; i < kiek; i++)
+                for (int j = i + 1; j < kiek; j++)
+                {
+                    if (z[i] >= z[j])
+                    {
+                        Zaidejas min = z[i];
+                        Zaidejas max = z[j];
+                        z[i] = max;
+                        z[j] = min;
+                    }
+                }
+        }
+    }
+    class Komanda
+    {
+        string pav;
+        const int CMax = 100;
+        private Zaidejas[] z;
+        private int kiek;
+
+        public Komanda()
+        {
+            pav = "";
+            z = new Zaidejas[CMax];
+        }
+        public void Deti(string pav)
+        {
+            this.pav = pav;
+        }
+        public void Dėti(Zaidejas ob) { z[kiek++] = ob; }
+        public int imtiKiek() { return kiek; }
+        public string imtiPav() { return pav; }
+    }
+    internal class Program
+    {
+        const string CFd = "..\\..\\U1.txt";
+        const string CFr = "..\\..\\Rez.txt";
+        static void Main(string[] args)
+        {
+            if (File.Exists(CFr))
+                File.Delete(CFr);
+            Futbolininkai Fut = new Futbolininkai();
+            Futbolininkai Fut2 = new Futbolininkai();
+            Futbolininkai Fut3 = new Futbolininkai();
+            Skaitymas(Fut, CFd);
+            Fut.Rikiuoti();
+            GeriausiasZaid(Fut, Fut2);
+            Spausdinti(Fut, CFr, "PRADINIAI DUOMENYS(SURIKIUOTI PAGAL KOMANDA IR PAVARDE)");
+            string[] komandos = new string[6];
+            komandos[0] = "";
+            AtrinktiKomandas(komandos, Fut);
+            AtrinktiZaidejus(komandos, Fut2, Fut3);
+            Fut3.Rikiuoti();
+            Spausdinti(Fut3, CFr, "ATRINKTI DUOMENYS(DIDŽAUSIA NAUDINGUMO KOEFICINTĄ TURINTYS ŽAIDĖJAI)");
+            using (var fr = File.AppendText(CFr))
+            {
+                fr.WriteLine("Vidutinis sužaistų rungtynių skaičius - {0,3}", VidRungt(Fut));
+            }
+        }
+        /// <summary>
+        /// skaitymo metodas
+        /// </summary>
+        /// <param name="Fut"></param>
+        /// <param name="fv"></param>
+        static void Skaitymas(Futbolininkai Fut, string fv)
+        {
+            string komanda, vardas, pavarde;
+            int RuSk, ImSk;
+            string[] lines = File.ReadAllLines(fv, Encoding.GetEncoding(1257));
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(';');
+                komanda = parts[0];
+                pavarde = parts[1];
+                vardas = parts[2];
+                RuSk = int.Parse(parts[3]);
+                ImSk = int.Parse(parts[4]);
+                Zaidejas z = new Zaidejas(komanda, pavarde, vardas, RuSk, ImSk);
+                Fut.Dėti(z);
+            }
+        }
+        /// <summary>
+        /// spausdinimo metodas
+        /// </summary>
+        /// <param name="fut"></param>
+        /// <param name="fv"></param>
+        /// <param name="antraštė"></param>
+        static void Spausdinti(Futbolininkai fut, string fv, string antraštė)
+        {
+
+            string virsus = "---------------------------------------------------" +
+                                "---------------------\r\n  Komanda    |     Pavarde   " +
+                                "  |  Vardas   |Sužaistos rungt.| Įvarčiai  |\r\n-" +
+                                "---------------------------------------------------" +
+                                "--------------------";
+            using (var fr = File.AppendText(fv))
+            {
+                fr.WriteLine(antraštė);
+                fr.WriteLine(virsus);
+
+                for (int i = 0; i < fut.imtiKiek(); i++)
+                    fr.WriteLine("{0}", fut.Imti(i).ToString());
+
+                fr.WriteLine("---------------------------------------------" +
+                                  "---------------------------\n\n");
+            }
+        }
+        /// <summary>
+        /// metodas skirtas apskaičiuoti kiek vidutiniškai žaidėjai sužaidė runtynių
+        /// </summary>
+        /// <param name="Fut"></param>
+        /// <returns></returns>
+        static double VidRungt(Futbolininkai Fut)
+        {
+            double laikinas = 0;
+            for (int i = 0; i < Fut.imtiKiek(); i++)
+            {
+                laikinas += Fut.Imti(i).ImtiRungt();
+            }
+            laikinas = laikinas / Fut.imtiKiek();
+            return laikinas;
+        }
+        /// <summary>
+        /// metodas skirtas surasti geriausią žaidėją
+        /// </summary>
+        /// <param name="Fut"></param>
+        /// <param name="Fut2"></param>
+        static void GeriausiasZaid(Futbolininkai Fut, Futbolininkai Fut2)
+        {
+            for (int i = 0; i < Fut.imtiKiek(); i++)
+            {
+                if (Fut.Imti(i).ImtiRungt() > VidRungt(Fut))
+                {
+                    Fut2.Dėti(Fut.Imti(i));
+                }
+            }
+        }
+        static void AtrinktiKomandas(string[] komPav, Futbolininkai Fut)
+        {
+            int j = 0;
+            for(int i = 0; i < Fut.imtiKiek(); i++)
+            {
+                if (!komPav.Contains(Fut.Imti(i).ImtiKomanda()))
+                {
+                    komPav[j] = Fut.Imti(i).ImtiKomanda();
+                    j++;
+                }
+            }
+        }
+        static void AtrinktiZaidejus(string[] komPav, Futbolininkai Fut2, Futbolininkai Fut3)
+        {
+            int ivarciai = 0;
+            int indeksas = 0;
+            foreach (string pavadinimas in komPav)
+            {
+                for (int i = 0; i < Fut2.imtiKiek(); i++)
+                {
+                        if (pavadinimas == Fut2.Imti(i).ImtiKomanda()
+                           && Fut2.Imti(i).ImtiIvarciai() > ivarciai)
+                        {
+                            ivarciai = Fut2.Imti(i).ImtiIvarciai();
+                            indeksas = i;
+                        }
+                }
+                if (Fut3.Imti(indeksas) != Fut2.Imti(indeksas))
+                {
+                    ivarciai = 0;
+                    Fut3.Dėti(Fut2.Imti(indeksas));
+                }
+            }
+        }
+    }
+}
---
+++ U4-2. Mobiliojo ryšio kortelės/U4-2. Mobiliojo ryšio kortelės/Program.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace U4_2.Mobiliojo_ryšio_kortelės
+{
+    /// <summary>
+    /// Kortelės klasė
+    /// </summary>
+    class Kortele
+    {
+        string pavadinimas; //Tinklo pavadinimas
+        double suma, //Pradinė suma
+            tarSav, //Skambučių tarifas savame tinkle
+            tarKit, //Skambučių tarifas kitame tinkle
+            SMStarSav, //Žinučių tarifas savame tinkle
+            SMStarKit; //Žinučių tarifas kitame tinkle
+        /// <summary>
+        /// Pradiniai kortelės duomenys
+        /// </summary>
+        public Kortele()
+        {
+            pavadinimas = "";
+            suma = 0;
+            tarSav = 0;
+            tarKit = 0;
+            SMStarSav = 0;
+            SMStarKit = 0;
+        }
+        /// <summary>
+        /// Kortelės įdėjimo į masyvą metodas
+        /// </summary>
+        /// <param name="pavadinimas"></param>
+        /// <param name="suma"></param>
+        /// <param name="tarSav"></param>
+        /// <param name="tarKit"></param>
+        /// <param name="SMStarSav"></param>
+        /// <param name="SMStarKit"></param>
+        public Kortele(string pavadinimas, double suma, double tarSav,
+            double tarKit, double SMStarSav, double SMStarKit)
+        {
+            this.pavadinimas = pavadinimas;
+            this.suma = suma;
+            this.tarSav = tarSav;
+            this.tarKit = tarKit;
+            this.SMStarSav = SMStarSav;
+            this.SMStarKit = SMStarKit;
+        }
+        /// <summary>
+        /// Užklotas ToString() metodas
+        /// </summary>
+        /// <returns>Grąžina pakeistą string</returns>
+        public override string ToString()
+        {
+            string eilute;
+            eilute = string.Format("{0, -12}   {1,-6:f2}             {2,-6:f2}" +
+                "             {3,-6:f2}              {4,-6:f2}     " +
+                "          {5,-6:f2}",
+                                   pavadinimas, suma, tarSav, tarKit, SMStarSav,
+                                   SMStarKit);
+            return eilute;
+        }
+        /// <summary>
+        /// Metodas, kuris grąžina tinklo pavadiniimą
+        /// </summary>
+        /// <returns></returns>
+        public string ImtPavadinima() { return pavadinimas; }
+        /// <summary>
+        /// Metodas, kuris grąžina pradinę sumą kortelėje
+        /// </summary>
+        /// <returns></returns>
+        public double ImtSuma() { return suma; }
+        /// <summary>
+        /// Metodas, kuris grąžina skambučių savame tinkle kainą
+        /// </summary>
+        /// <returns></returns>
+        public double ImtTarSav() { return tarSav; }
+        /// <summary>
+        /// Metodas, kuris grąžina skambučių kitame tinkle kainą
+        /// </summary>
+        /// <returns></returns>
+        public double ImtTarKit() { return tarKit; }
+        /// <summary>
+        /// Metodas, kuris grąžina SMS savame tinkle kainą
+        /// </summary>
+        /// <returns></returns>
+        public double ImtSMStarSav() { return SMStarSav; }
+        /// <summary>
+        /// Metodas, kuris grąžina SMS kitame tinkle kainą
+        /// </summary>
+        /// <returns></returns>
+        public double ImtSMStarKit() { return SMStarKit; }
+        /// <summary>
+        /// <=palyginimo operatorius
+        /// </summary>
+        /// <param name="kortele1"></param>
+        /// <param name="kortele2"></param>
+        /// <returns></returns>
+        public static bool operator <=(Kortele kortele1, Kortele kortele2)
+        {
+            int abecele = String.Compare(kortele1.pavadinimas,
+                                             kortele2.pavadinimas,
+                                             StringComparison.CurrentCulture);
+
+            return (kortele1.suma < kortele2.suma ||
+                    ((kortele1.suma == kortele2.suma) && (abecele > 0)));
+        }
+        /// <summary>
+        /// >=palyginimo operatorius
+        /// </summary>
+        /// <param name="kortele1"></param>
+        /// <param name="kortele2"></param>
+        /// <returns></returns>
+        public static bool operator >=(Kortele kortele1, Kortele kortele2)
+        {
+            int abecele = String.Compare(kortele1.pavadinimas,
+                                             kortele2.pavadinimas,
+                                             StringComparison.CurrentCulture);
+
+            return (kortele1.suma > kortele2.suma ||
+                    ((kortele1.suma == kortele2.suma) && (abecele < 0)));
+        }
+
+
+    }
+    /// <summary>
+    /// Kortelių konteineris
+    /// </summary>
+    class KortKon
+    {
+        const int CMaxi = 100;
+        private Kortele[] kortele;
+        private int kiek;
+        /// <summary>
+        /// Pradinės konteinerio reikšmės
+        /// </summary>
+        public KortKon()
+        {
+            kiek = 0;
+            kortele = new Kortele[CMaxi];
+        }
+        /// <summary>
+        /// Metodas, kuris paima informaciją iš kortelės klasės
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public Kortele Imti(int i) { return kortele[i]; }
+        /// <summary>
+        /// Metodas, kuris grąžina įrašų kiekį masyve
+        /// </summary>
+        /// <returns></returns>
+        public int Imti() { return kiek; }
+        /// <summary>
+        /// Metodas, kuris į konteinerį įdeda naują įrašą
+        /// </summary>
+        /// <param name="ob"></param>
+        public void Deti(Kortele ob) { kortele[kiek++] = ob; }
+        /// <summary>
+        /// Metodas, kuris surūšiuoja konteinerį abecelės tvarka ir
+        /// pradinę sumą mažėjant
+        /// </summary>
+        public void Rusiavimas()
+        {
+            for (int i = 0; i < kiek - 1; i++)
+            {
+                Kortele kitas = kortele[i];
+                int im = i;
+                for (int j = i + 1; j < kiek; j++)
+                {
+                    if (kortele[j] <= kitas)
+                    {
+                        kitas = kortele[j];
+                        im = j;
+                    }
+                }
+                kortele[im] = kortele[i];
+                kortele[i] = kitas;
+            }
+        }
+
+
+    }
+    internal class Program
+    {
+        const string CFd = "..\\..\\Duomenys.txt"; //Nuorodą į duomenų failą
+        const string CFr = "..\\..\\Rezultatai.txt"; //Nuoroda į rezultatų failą
+        static void Main(string[] args)
+        {
+            KortKon kortKon = new KortKon(); //Sukuriamas naujas konteineris
+
+            Skaityti(ref kortKon, CFd); //Skaitoma informacija iš failo ir įrašoma
+                                        //į konteinerį
+
+            using (var fr = File.CreateText(CFr)) { fr.WriteLine(""); }
+                if (kortKon.Imti() > 0) //Spausdinama lentelė, jeigu konteineryje
+            {                       //yra įrašų
+                Spausdinti(CFr ,kortKon);
+            }
+            else
+            {
+                using (var fr = File.AppendText(CFr))
+                {
+                    fr.WriteLine("Nėra mobilaus ryšio operatorių");
+                }
+            }
+            //Spausdinami tinklo operatoriai, kurių sms tarifai į kitus tinklus yra
+            //mažiausi
+            SpausdintiMaz(CFr, kortKon, kortKon.Imti(IeskotiSMSMaz(kortKon)).ImtSMStarKit());
+
+            KortKon naujosKorteles = new KortKon(); //Sukuriamas naujas konteineris
+
+            //Iš seno konteinerio įvedama informacija atitinkanti užduoties sąlygas
+            KorteliuAtrinkimas(kortKon, ref naujosKorteles);
+            //Tikrinama ar konteineriai vienodi
+            if (kortKon.Imti().Equals(naujosKorteles.Imti()))
+            {
+                Console.WriteLine("Visi mobilaus ryšio operatoriai leidžia nemokamai" +
+                    " skambinti ir rašyti žinutes į savo tinklą.");
+            }
+
+            naujosKorteles.Rusiavimas(); //Pertvarkomas naujas konteineris
+
+            if (naujosKorteles.Imti() > 0) //Spausdinama lentelė, jeigu konteineryje
+            {                              //į konteinerį
+                Spausdinti(CFr, naujosKorteles);
+            }
+            else
+            {
+                using (var fr = File.AppendText(CFr))
+                {
+                    fr.WriteLine("Nėra tinkamų mobilaus ryšio operatorių");
+                }
+            }
+        }
+        /// <summary>
+        /// Skaitymo metodas
+        /// </summary>
+        /// <param name="kortKon">Kortelių konteineris</param>
+        /// <param name="fv">Duomenų failo nuoroda</param>
+        static void Skaityti(ref KortKon kortKon, string fv)
+        {
+            string pavadinimas;
+            double suma,
+                tarSav,
+                tarKit,
+                SMStarSav,
+                SMStarKit;
+
+            string[] eilutes = File.ReadAllLines(fv, Encoding.GetEncoding(1257));
+
+            using (StreamReader reader = new StreamReader(fv))
+            {
+                foreach (string eilute in eilutes)
+                {
+                    string[] parts = eilute.Split(';');
+                    pavadinimas = parts[0].Trim();
+                    suma = double.Parse(parts[1].Trim());
+                    tarSav = double.Parse(parts[2].Trim());
+                    tarKit = double.Parse(parts[3].Trim());
+                    SMStarSav = double.Parse(parts[4].Trim());
+                    SMStarKit = double.Parse(parts[5].Trim());
+
+                    Kortele kort = new Kortele(pavadinimas, suma, tarSav, tarKit,
+                        SMStarSav, SMStarKit);
+                    kortKon.Deti(kort);
+                }
+            }
+        }
+        /// <summary>
+        /// Lentelės spausdinimo metodas
+        /// </summary>
+        /// <param name="kortKon">Kortelių konteineris</param>
+        static void Spausdinti(string fv, KortKon kortKon)
+        {
+            string virsus = "                         Informacija apie MRO \r\n" +
+                            "--------------------------------------------------" +
+                            "--------------------------------------------------" +
+                            "--------" +
+                            "\r\nTinklas  /  Pradinė Suma  /  Lokalus Tarifas /" +
+                            " Išorinis Tarifas / Lokalus SMS Tarifas /" +
+                            " Išorinis SMS Tarifas\r\n" +
+                            "--------------------------------------------------" +
+                            "--------------------------------------------------" +
+                            "--------";
+
+            using (var fr = File.AppendText(fv))
+            {
+                fr.WriteLine(virsus);
+
+                for (int i = 0; i < kortKon.Imti(); i++)
+                    fr.WriteLine("{0}", kortKon.Imti(i).ToString());
+
+                fr.WriteLine("--------------------------------------------------" +
+                                "--------------------------------------------------" +
+                                "--------\n");
+            }
+        }
+        /// <summary>
+        /// Metodas, kuris atrenka tinklo operatorius su
+        /// mažiausiais SMS tarifais į kitus tarifus
+        /// </summary>
+        /// <param name="kortKon">Kortelių konteineris</param>
+        /// <returns>Grąžinama pozicija konteineryje</returns>
+        static int IeskotiSMSMaz(KortKon kortKon)
+        {
+            double maziausias = 1;
+            int mazInt = 0;
+
+            for (int i = 0; i < kortKon.Imti(); i++)
+            {
+                if (kortKon.Imti(i).ImtSMStarKit() < maziausias &&
+                    kortKon.Imti(i).ImtSMStarKit() != 0)
+                {
+                    maziausias = kortKon.Imti(i).ImtSMStarKit();
+                    mazInt = i;
+                }
+            }
+            return mazInt;
+        }
+        /// <summary>
+        /// Metodas, kuris atrenka įrašus į naują konteinerį pagal užduoties
+        /// sąlygą
+        /// </summary>
+        /// <param name="kortKon">Pradinis kortelių konteineris</param>
+        /// <param name="naujosKorteles">Naujas kortelių konteineris</param>
+        static void KorteliuAtrinkimas(KortKon kortKon, ref KortKon naujosKorteles)
+        {
+            Kortele kort;
+            for (int i = 0; i < kortKon.Imti(); i++)
+            {
+                if (kortKon.Imti(i).ImtSMStarSav() == 0 &&
+                    kortKon.Imti(i).ImtTarSav() == 0)
+                {
+                    kort = kortKon.Imti(i);
+                    naujosKorteles.Deti(kort);
+                }
+            }
+        }
+        /// <summary>
+        /// Metodas, kuris spausdina IeskotiSMSMaz() metodo atrinktą
+        /// informaciją
+        /// </summary>
+        /// <param name="kortKon">Pradinis kortelių konteineris</param>
+        /// <param name="maziausias">Mažiausia SMS tarifo į kitus reikšmė</param>
+        static void SpausdintiMaz(string fv, KortKon kortKon, double maziausias)
+        {
+            using (var fr = File.AppendText(fv))
+            {
+                fr.WriteLine("Kortelės su mažiausiais mokamais SMS tarifais į kitus tinklus:");
+                fr.WriteLine("Mažiausias mokamas tarifas {0,-6:f2}", maziausias);
+                for (int i = 0; i < kortKon.Imti(); i++)
+                {
+                    if (kortKon.Imti(i).ImtSMStarKit() == maziausias)
+                    {
+                        fr.WriteLine("{0}", kortKon.Imti(i).ToString());
+                    }
+                }
+                fr.WriteLine("");
+            }
+        }
+    }
+}
